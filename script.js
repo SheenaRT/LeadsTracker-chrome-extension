b16d3e737a2ce3c5912a5c06a5e481bcbe2b6a9d
Sheena Thomas <sheena@missionreadyhq.com>
2023-06-04 22:38:05 +1200

    added more html, css  javascript

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 let myLeads = []
 
+myLeads = JSON.stringify(myLeads)
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEL = document.getElementById("ul-el")
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 inputBtn.addEventListener ("click", function() {
-    console.log("Button clicked!")
+    myLeads.push(inputEl.value);
+    inputEl.value = ""; // clears input field 
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads();
 });
+
+function renderLeads() {
+let listItems = ""
+for (let i = 0; i < myLeads.length; i++) {
+    // ulEL.innerHTML += `<li>${myLeads[i]}</li>`;
+    /*Another way of writing the above:
+    const li = document.createElement("li")
+    li.textContent = myLeads[i]
+    ulEl.append(li)*/
+    listItems += `
+    <li>
+    <a target= "_blank" href = "${myLeads[i]}">
+    ${myLeads[i]}
+    </a>
+    </li>
+    `;
+    //"<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+}
+ulEL.innerHTML = listItems;
+}
